33cb8c9ae8eca3e98ab7f282fb6fb7aa770f0016
Ivan Bogomolov <qhomop@gmail.com>
2023-03-31 04:02:28 +0300

    fix clear context

--- app/BinanceAPI/binanceAPI.py
+++ app/BinanceAPI/binanceAPI.py
@@ -1,8 +1,13 @@
 from app.SingletonWrapper import singleton
-from binance import Client
-from binance.exceptions import BinanceAPIException
+# from binance import Client
+# from binance.exceptions import BinanceAPIException
 
 @singleton
 class BinanceAPI:
     def __init__(self):
-        pass
+        self.a = 1
+
+
+
+c = BinanceAPI()
+print(c.a)
